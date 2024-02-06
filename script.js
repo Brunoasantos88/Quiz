cc7bf28f7dc85c9820f6153681f647d4624b008e
Bruno Alex <142893092+Brunoasantos88@users.noreply.github.com>
2024-02-06 11:41:52 -0300

    Segunda aula

--- script.js
+++ script.js
@@ -96,6 +96,11 @@ const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 const quizItem = template.content.cloneNode(true)
 
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostarTotal = document.querySelector('#acertos span')
+mostarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+
 for(const item of perguntas) {
     const quizItem = template.content.cloneNode(true)
     quizItem.querySelector('h3').textContent = item.pergunta
@@ -104,7 +109,24 @@ for(const item of perguntas) {
     for(let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
         dt.querySelector('span').textContent = resposta
+        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+        dt.querySelector('input').onchange = (event) => {
+           const estaCorreta = event.target.value == item.correta
+
+           corretas.delete(item)
+           if(estaCorreta) {
+            corretas.add(item)
+           }
+           
+           mostarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+        }
+
+
+
         quizItem.querySelector('dl').appendChild(dt)
+
+        
     }
 
 quizItem.querySelector('dl dt').remove()
